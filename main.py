f9fb8f8f1d2d2d6caed73177f1bfee8d9e9116f2
Anteneh Mulu <anteneh.mulu1921@gmail.com>
2025-02-01 10:17:39 +0300

    Sending Mail for Different reciept from Template

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-# file=open("my_file.txt")
-# content=file.read()
-# print(content)
-with open("my_file.txt",mode="a") as file:
-    file.write("\nAppending the text")
-file.close()
+# TODO: Create a letter using starting_letter.txt
+with open("./Input/Letters/starting_letter.txt") as letter:
+    letter_greeting=letter.readline()
+    letter_full=letter.read()
+
+with open("./Input/Names/invited_names.txt") as name:
+    lines = [line.rstrip() for line in name]
+    for recipient_name in lines:
+        x=letter_greeting.replace("[name]", recipient_name)
+        ready_mail=f"{x}{letter_full}"
+        with open(f"./Output/ReadyToSend/Mail_for_{recipient_name}.txt",mode='w') as mail:
+            mail.write(ready_mail)
+
+
+
+
+
+
